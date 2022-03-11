c189fc2d32c66a6b31278846a9d477de3e0cadd4
Nathan Nelson <nathan.nelson042@gmail.com>
2022-03-10 17:28:45 -0700

    added last of the models. vots still needs to be finished

---
+++ models/Vote.js
@@ -0,0 +1,14 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+
+
+// create a vote class with id, post id
+
+
+
+
+
+
+module.exports = Vote;
